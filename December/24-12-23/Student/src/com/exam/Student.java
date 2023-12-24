76ed5fb9041ceb350c48f7b90ab09868e82ccc23
Sazid Mahmud <45362238+sazidmahmud36@users.noreply.github.com>
2023-12-24 12:49:09 +0600

    Add files via upload

---
+++ December/24-12-23/Student/src/com/exam/Student.java
@@ -0,0 +1,19 @@
+package com.exam;
+
+
+public class Student {
+    String name = "Sazid";
+    int id = 101;
+    int batch = 57;
+    
+    public static String m1(){
+        return "Hello";
+    }
+    public static void m2(){
+        System.out.println("Korim");
+    }
+    
+    public void m3(){
+        System.out.println(name);
+    }
+}
---
+++ December/24-12-23/Student/src/com/exam/Test.java
@@ -0,0 +1,14 @@
+
+package com.exam;
+
+public class Test {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        
+        System.out.println(s1.name);
+        System.out.println(s1.m1());
+        s1.m2();
+        s1.m3();
+        System.out.println(s1.id);
+    }
+}
